8d95076b058d3e416d8d9dc633a84b9b68a0e69f
Juan Yaguaro <jpyaguaro@gmail.com>
2021-01-10 07:29:28 -0400

    Add default ctor and exception when invalid arguments are received

--- include/cachesim/cache.h
+++ include/cachesim/cache.h
@@ -3,6 +3,7 @@
 #define CACHESIM_CACHE_H_
 
 #include <cstddef>
+#include <exception>
 
 namespace cachesim {
 
@@ -13,7 +14,7 @@ enum emplace_policy { LRU, MRU };
 class cache {
  public:
   // ctor
-  cache() = default;
+  cache();
   explicit cache(const std::size_t& size, const std::size_t& line_size,
                  emplace_policy& policy);
   // dtor
@@ -29,17 +30,30 @@ class cache {
   virtual void emplace(const int& value) = 0;
 
  protected:
+  bool is_pow2(const std::size_t& n) const noexcept;
+  // member variables
   std::size_t size_;       // cache size
   std::size_t line_size_;  // cache line size
   emplace_policy policy_;  // emplace policy
 };
 
+cache::cache() : size_(1), line_size_(1), policy_(LRU) {}
+
 cache::cache(const std::size_t& size, const std::size_t& line_size,
              emplace_policy& policy)
-    : size_(size), line_size_(line_size), policy_(policy) {}
+    : size_(size), line_size_(line_size), policy_(policy) {
+  if (!is_pow2(size) || !is_pow2(line_size) || line_size > size) {
+    throw std::invalid_argument("Invalid size");
+  }
+}
 
 std::size_t cache::size() const noexcept { return size_; }
+
 std::size_t cache::line_size() const noexcept { return line_size_; }
+
+bool cache::is_pow2(const std::size_t& n) const noexcept {
+  return n && !(n & (n - 1));
+}
 
 }  // namespace cachesim
 
