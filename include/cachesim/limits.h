46e7c6f08471ad7fa50eda44c234f14e042f4eef
Juan Yaguaro <jpyaguaro@gmail.com>
2021-01-18 18:43:30 -0400

    Add limits header

---
+++ include/cachesim/limits.h
@@ -0,0 +1,14 @@
+// Copyright 2020 Juan Yaguaro
+#ifndef CACHESIM_LIMITS_H_
+#define CACHESIM_LIMITS_H_
+
+#include <cstddef>
+
+namespace cachesim {
+namespace limits {
+constexpr std::size_t pow_min = 0;
+constexpr std::size_t pow_max = 16;
+}  // namespace limits
+}  // namespace cachesim
+
+#endif  // CACHESIM_LIMITS_H_
