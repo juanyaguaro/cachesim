f34a4547ec1f96abd00b4c8915997dc9a02bd71d
Juan Yaguaro <jpyaguaro@gmail.com>
2021-01-18 20:28:41 -0400

    Add iteration and numeric min/max

--- include/cachesim/limits.h
+++ include/cachesim/limits.h
@@ -3,11 +3,16 @@
 #define CACHESIM_LIMITS_H_
 
 #include <cstddef>
+#include <limits>
 
 namespace cachesim {
 namespace limits {
-constexpr std::size_t pow_min = 0;
-constexpr std::size_t pow_max = 16;
+constexpr const std::size_t it_min = 1;
+constexpr const std::size_t it_max = 100;
+constexpr const std::size_t num_min = 0;
+constexpr const std::size_t num_max = 65535;
+constexpr const std::size_t pow_min = 0;
+constexpr const std::size_t pow_max = 16;
 }  // namespace limits
 }  // namespace cachesim
 
