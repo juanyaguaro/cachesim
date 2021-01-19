2e8d4b1f827a0f88187644aaacf7acbeb629a728
Juan Yaguaro <jpyaguaro@gmail.com>
2021-01-19 14:13:36 -0400

    Add comments

--- include/cachesim/version.h
+++ include/cachesim/version.h
@@ -4,21 +4,15 @@
 
 namespace cachesim {
 
+// cachesim --version output.
 constexpr const char* cachesim_version =
     "cachesim " CACHESIM_VERSION_H_ "\nCopyright 2020 Juan Yaguaro.\n";
 
+// test_generator --version output.
 constexpr const char* test_generator_version =
     "test_generator " CACHESIM_VERSION_H_ "\nCopyright 2020 Juan Yaguaro.\n";
 
-constexpr const char* test_generator_help =
-    "Usage: test_generator -c=[VALUE] -d=[OPTION]\n"
-    "\t-c=[VALUE]\t\tfilename for config file.\n"
-    "\t-d=[VALUE]\t\tfilename for data file.\n"
-    "\t-h, --help\t\tdisplay all available commands.\n"
-    "\t-v, --version\t\tdisplay version of test_generator.\n"
-    "Full documentation at: <https://github.com/juanyaguaro/cachesim>.\n"
-    "Or available locally in <docs> folder.\n";
-
+// cachesim --help output.
 constexpr const char* cachesim_help =
     "Usage: cachesim -c=[VALUE] -d=[OPTION] -o=[OPTION]\n"
     "\t-c=[VALUE]\t\tfilename for config file.\n"
@@ -29,14 +23,26 @@ constexpr const char* cachesim_help =
     "Full documentation at: <https://github.com/juanyaguaro/cachesim>.\n"
     "Or available locally in <docs> folder.\n";
 
-constexpr const char* test_generator_default =
-    "test_generator - generates input files for cachesim.\n"
-    "Try [test_generator -h] or [test_generator --help] for help.\n";
+// test_generator --help output.
+constexpr const char* test_generator_help =
+    "Usage: test_generator -c=[VALUE] -d=[OPTION]\n"
+    "\t-c=[VALUE]\t\tfilename for config file.\n"
+    "\t-d=[VALUE]\t\tfilename for data file.\n"
+    "\t-h, --help\t\tdisplay all available commands.\n"
+    "\t-v, --version\t\tdisplay version of test_generator.\n"
+    "Full documentation at: <https://github.com/juanyaguaro/cachesim>.\n"
+    "Or available locally in <docs> folder.\n";
 
+// cachesim default output.
 constexpr const char* cachesim_default =
     "cachesim - simulates cache behavior with given configuration and values "
     "to allocate.\n"
     "Try [cachesim -h] or [cachesim --help] for help.\n";
+
+// test_generator default output.
+constexpr const char* test_generator_default =
+    "test_generator - generates input files for cachesim.\n"
+    "Try [test_generator -h] or [test_generator --help] for help.\n";
 
 }  // namespace cachesim
 
