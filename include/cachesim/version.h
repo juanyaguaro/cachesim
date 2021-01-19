4e641d14028ce865aeaa5e3d6b6a27f32c3731c6
Juan Yaguaro <jpyaguaro@gmail.com>
2021-01-18 23:47:08 -0400

    Add cachesim_default, cachesim_help constants

--- include/cachesim/version.h
+++ include/cachesim/version.h
@@ -19,9 +19,24 @@ constexpr const char* test_generator_help =
     "Full documentation at: <https://github.com/juanyaguaro/cachesim>.\n"
     "Or available locally in <docs> folder.\n";
 
+constexpr const char* cachesim_help =
+    "Usage: cachesim -c=[VALUE] -d=[OPTION] -o=[OPTION]\n"
+    "\t-c=[VALUE]\t\tfilename for config file.\n"
+    "\t-d=[VALUE]\t\tfilename for data file.\n"
+    "\t-d=[VALUE]\t\tfilename for output file (default value is stdout).\n"
+    "\t-h, --help\t\tdisplay all available commands.\n"
+    "\t-v, --version\t\tdisplay version of test_generator.\n"
+    "Full documentation at: <https://github.com/juanyaguaro/cachesim>.\n"
+    "Or available locally in <docs> folder.\n";
+
 constexpr const char* test_generator_default =
     "test_generator - generates input files for cachesim.\n"
     "Try [test_generator -h] or [test_generator --help] for help.\n";
+
+constexpr const char* cachesim_default =
+    "cachesim - simulates cache behavior with given configuration and values "
+    "to allocate.\n"
+    "Try [cachesim -h] or [cachesim --help] for help.\n";
 
 }  // namespace cachesim
 
